77a67359d6d946a425e71c5f5fc6aa2ca85f2c28
Jorge Luis Palacios Dalo <jorge.palacios@skyairline.com>
2020-09-05 19:43:41 -0400

    :sparkles: job filter by category feature

---
+++ src/config/jobs.js
@@ -0,0 +1,14 @@
+export const JOBS_CATEGORIES = {
+  programming: {
+    title: 'Programming',
+    id: 'programming'
+  },
+  'design-ux': {
+    title: 'Design/UX',
+    id: 'design-ux'
+  },
+  'default': {
+    title: 'All Jobs',
+    id: 'default'
+  },
+}
--- src/containers/JobsContainer.js
+++ src/containers/JobsContainer.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import JobItem from '../components/JobItem'
 import { getAllJobs } from '../utils/getJobs'
+import { JOBS_CATEGORIES } from '../config/jobs'
 
 const JobsContainer = () => {
   
   const [jobs, setJobs] = useState([])
   const [selectedJobs, setSelectedJobs] = useState([])
   const [selectedJobsTotal, setSelectedJobsTotal] = useState(0)
-  const [category, setCategory] = useState(0)
+  const [category, setCategory] = useState(JOBS_CATEGORIES.default.id)
 
   const getJobs = async () => {
     const fetchedJobs = await getAllJobs()
@@ -15,7 +16,7 @@ const JobsContainer = () => {
     setJobs(fetchedJobs)
     setSelectedJobs(fetchedJobs)
     setSelectedJobsTotal(fetchedJobs.length)
-    setCategory('all')
+    setCategory(JOBS_CATEGORIES.default.id)
   }
 
   const removeJob = (id) => {
@@ -24,25 +25,47 @@ const JobsContainer = () => {
     setSelectedJobsTotal(selectedJobsTotal - 1)
   }
 
+  const changeCategory = (category) => {
+    setCategory(category)
+    if (category === JOBS_CATEGORIES.default.id) {
+      setSelectedJobs(jobs)
+      setSelectedJobsTotal(jobs.length)
+      return;
+    }
+
+    const jobsInCategory = jobs.filter(job => job.category.name === category)
+    setSelectedJobsTotal(jobsInCategory.length)
+    setSelectedJobs(jobsInCategory)
+  }
+
   useEffect(() => {
     getJobs()
   }, [])
 
+  const getListTitle = (category) => category !== JOBS_CATEGORIES.default.id ? `${JOBS_CATEGORIES[category].title} Jobs` : 'Jobs'
+
   return (
     <div className="jobs-list">
       <div className="jobs-list__header">
-        <div className="jobs-list__title">{`Jobs list (${selectedJobsTotal})`}</div>
-        <select className="">
-          <option value="all">All jobs</option>
-          <option value="programming">Programming</option>
-          <option value="design">Design</option>
+        <div className="jobs-list__title">
+          {`${getListTitle(category)} (${selectedJobsTotal})`}
+        </div>
+        <select className="" onChange={(event) => {changeCategory(event.target.value)}}>
+          <option value="default">{JOBS_CATEGORIES.default.title}</option>
+          {
+            Object.values(JOBS_CATEGORIES).map(
+              job => job.id !== 'default'
+              ? (<option key={job.id} value={job.id}>{job.title}</option>)
+              : ''
+            )
+          }
         </select>
       </div>
       <div className="jobs-list__results">
         {jobs.length > 0 ? selectedJobs.map((job) => (
           <JobItem key={job.id} jobData={job} removeJob={removeJob}/>
         ))
-          : <div>No items</div>}
+          : <div className="">No items</div>}
       </div>
     </div>
   )
