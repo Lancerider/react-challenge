47586d39619cb1131986fa4248abfbbbd0cf3fbc
Jorge Luis Palacios Dalo <jorge.palacios@skyairline.com>
2020-09-05 20:35:16 -0400

    :sparkles: Loader and NoItems setted

---
+++ src/components/Loader.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import '../assets/styles/components/loader.css'
+
+class Loader extends React.Component {
+  render() {
+    return (
+      <div className="loader-grid">
+        <div/>
+        <div/>
+        <div/>
+        <div/>
+        <div/>
+        <div/>
+        <div/>
+        <div/>
+        <div/>
+      </div>
+    )
+  }
+}
+
+export default Loader
--- src/containers/JobsContainer.js
+++ src/containers/JobsContainer.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react'
+
 import JobItem from '../components/JobItem'
+import Loader from '../components/Loader'
 import { getAllJobs } from '../utils/getJobs'
 import { JOBS_CATEGORIES } from '../config/jobs'
 
 const JobsContainer = () => {
-  
   const [jobs, setJobs] = useState([])
   const [selectedJobs, setSelectedJobs] = useState([])
   const [selectedJobsTotal, setSelectedJobsTotal] = useState(0)
   const [category, setCategory] = useState(JOBS_CATEGORIES.default.id)
+  const [isLoading, setIsLoading] = useState(false)
 
   const getJobs = async () => {
     const fetchedJobs = await getAllJobs()
-    console.log("Console log : getJobs -> fetchedJobs", fetchedJobs)
+    setIsLoading(false)
+    console.log('Console log : getJobs -> fetchedJobs', fetchedJobs)
     setJobs(fetchedJobs)
     setSelectedJobs(fetchedJobs)
     setSelectedJobsTotal(fetchedJobs.length)
@@ -20,8 +23,8 @@ const JobsContainer = () => {
   }
 
   const removeJob = (id) => {
-    const remainingJobs = selectedJobs.filter(job => job.id !== id)
-    setSelectedJobs (remainingJobs)
+    const remainingJobs = selectedJobs.filter((job) => job.id !== id)
+    setSelectedJobs(remainingJobs)
     setSelectedJobsTotal(selectedJobsTotal - 1)
   }
 
@@ -30,19 +33,23 @@ const JobsContainer = () => {
     if (category === JOBS_CATEGORIES.default.id) {
       setSelectedJobs(jobs)
       setSelectedJobsTotal(jobs.length)
-      return;
+      return
     }
 
-    const jobsInCategory = jobs.filter(job => job.category.name === category)
+    const jobsInCategory = jobs.filter((job) => job.category.name === category)
     setSelectedJobsTotal(jobsInCategory.length)
     setSelectedJobs(jobsInCategory)
   }
 
   useEffect(() => {
+    setIsLoading(true)
     getJobs()
   }, [])
 
-  const getListTitle = (category) => category !== JOBS_CATEGORIES.default.id ? `${JOBS_CATEGORIES[category].title} Jobs` : 'Jobs'
+  const getListTitle = (category) =>
+    category !== JOBS_CATEGORIES.default.id
+      ? `${JOBS_CATEGORIES[category].title} Jobs`
+      : 'Jobs'
 
   return (
     <div className="jobs-list">
@@ -50,23 +57,39 @@ const JobsContainer = () => {
         <div className="jobs-list__title">
           {`${getListTitle(category)} (${selectedJobsTotal})`}
         </div>
-        <select className="" onChange={(event) => {changeCategory(event.target.value)}}>
+        <select
+          className="jobs-list__select"
+          onChange={(event) => {
+            changeCategory(event.target.value)
+          }}
+        >
           <option value="default">{JOBS_CATEGORIES.default.title}</option>
-          {
-            Object.values(JOBS_CATEGORIES).map(
-              job => job.id !== 'default'
-              ? (<option key={job.id} value={job.id}>{job.title}</option>)
-              : ''
+          {Object.values(JOBS_CATEGORIES).map((job) =>
+            job.id !== 'default' ? (
+              <option key={job.id} value={job.id}>
+                {job.title}
+              </option>
+            ) : (
+              ''
             )
-          }
+          )}
         </select>
       </div>
-      <div className="jobs-list__results">
-        {jobs.length > 0 ? selectedJobs.map((job) => (
-          <JobItem key={job.id} jobData={job} removeJob={removeJob}/>
-        ))
-          : <div className="">No items</div>}
-      </div>
+      {isLoading ? (
+        <div className="container">
+          <Loader />
+        </div>
+      ) : jobs.length > 0 ? (
+        <div className="jobs-list__results">
+          {selectedJobs.map((job) => (
+            <JobItem key={job.id} jobData={job} removeJob={removeJob} />
+          ))}
+        </div>
+      ) : (
+        <div className="container">
+          <div class="jobs-list__no-item">No items</div>
+        </div>
+      )}
     </div>
   )
 }
